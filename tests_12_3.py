a4e6f91608d7dc6f8db070a71a94dbe8d6d523ad
ArtemNikolavnin <me@nikolavnin.ru>
2024-12-13 15:31:30 +0300

    tests_12_3.py

--- tests_12_3.py
+++ tests_12_3.py
@@ -1,24 +1,33 @@
 import unittest
 from runner_and_tournament import Runner, Tournament
 
+def run_if_not_frozen(func):
+    def wrapper(self, *args, **kwargs):
+        if not self.is_frozen:
+            return func(self, *args, **kwargs)
+        else:
+            return unittest.skip("Тесты в этом кейсе заморожены")(func)(self, *args, **kwargs)
+    return wrapper
+
+
 class RunnerTest(unittest.TestCase):
     is_frozen = False
 
-    @unittest.skipIf(False, 'Тесты в этом кейсе заморожены')
+    @run_if_not_frozen
     def test_walk(self):
         runner = Runner("Артем")
         for _ in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
-    @unittest.skipIf(False, 'Тесты в этом кейсе заморожены')
+    @run_if_not_frozen
     def test_run(self):
         runner = Runner("Игорь")
         for _ in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
-    @unittest.skipIf(False, 'Тесты в этом кейсе заморожены')
+    @run_if_not_frozen
     def test_challenge(self):
         runner1 = Runner("Наташа")
         runner2 = Runner("Полина")
@@ -30,42 +39,45 @@ class RunnerTest(unittest.TestCase):
 
 class TournamentTest(unittest.TestCase):
     is_frozen = True
+    all_results = {}
 
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
+
+    @run_if_not_frozen
+    def test_usain_and_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        result = tournament.start()
+        TournamentTest.all_results["test_usain_and_nick"] = result
+        self.assertEqual(list(result.values())[-1].name, 'Ник')
+
+    @run_if_not_frozen
+    def test_andrey_and_nick(self):
+        tournament = Tournament(90, self.andrey, self.nick)
+        result = tournament.start()
+        TournamentTest.all_results["test_andrey_and_nick"] = result
+        self.assertEqual(list(result.values())[-1].name, 'Ник')
+
+    @run_if_not_frozen
+    def test_all_runners(self):
+        tournament = Tournament(90, self.usain, self.andrey, self.nick)
+        result = tournament.start()
+        TournamentTest.all_results["test_all_runners"] = result
+        self.assertEqual(list(result.values())[-1].name, 'Ник')
 
     def setUp(self):
         self.usain = Runner("Усэйн", 10)
         self.andrey = Runner("Андрей", 9)
         self.nick = Runner("Ник", 3)
 
-    def tearDownClass():
-        print("\nРезультаты всех тестов:")
-        for key, value in TournamentTest.all_results.items():
+    @classmethod
+    def tearDownClass(cls):
+        for key, value in cls.all_results.items():
             formatted_result = {k: v.name for k, v in value.items()}
             print(formatted_result)
 
-    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
-    def test_third_tournamen(self):
-        tournament = Tournament(90, self.usain, self.nick)
-        result = tournament.start()
-        TournamentTest.all_results["test_usain_and_nick"] = result
-        self.assertEqual(list(result.values())[-1].name, 'Ник')
-
-    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
-    def test_second_tournament(self):
-        tournament = Tournament(90, self.andrey, self.nick)
-        result = tournament.start()
-        TournamentTest.all_results["test_andrey_and_nick"] = result
-        self.assertEqual(list(result.values())[-1].name, 'Ник')
-
-    @unittest.skipIf(True, 'Тесты в этом кейсе заморожены')
-    def test_first_tournament(self):
-        tournament = Tournament(90, self.usain, self.andrey, self.nick)
-        result = tournament.start()
-        TournamentTest.all_results["test_all_runners"] = result
-        self.assertEqual(list(result.values())[-1].name, 'Ник')
 
 if __name__ == '__main__':
     unittest.main()
+
